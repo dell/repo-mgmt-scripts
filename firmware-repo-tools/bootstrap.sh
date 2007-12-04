1fab1390dde19ea4fa4ce9973d589d1d47ab7a43
Matt Domsch <Matt_Domsch@dell.com>
2007-12-04 13:49:15 -0600

    detect Ubuntu in firmware bootstrap.sh

--- firmware-repo-tools/bootstrap.sh
+++ firmware-repo-tools/bootstrap.sh
@@ -34,7 +34,7 @@ fi
 REPO_ID=FIRMWARE
 
 # packages are now signed by libsmbios key which should already be installed
-#GPG_KEY[0]=${SERVER}/${REPO_URL}/RPM-GPG-KEY-fwupdate
+#GPG_KEY[0]=${SERVER}/${REPO_URL}/RPM-GPG-KEY-libsmbios
 #GPG_KEY[3]=URL_OF_ADDITIONAL_GPG_KEYS_unless_not_using
 
 # change to 0 to disable check of repository RPM sig.
@@ -50,6 +50,10 @@ REPO_NAME="dell-firmware"
 
 #set -e
 #set -x
+
+unset DISTRIB_ID
+unset DISTRIB_CODENAME
+[ -r /etc/lsb-release ] && . /etc/lsb-release
 
 function write_mirror_cfg() 
 {
@@ -81,6 +85,7 @@ function distro_version()
 {
     # What distribution are we running?
     dist=unknown
+    [ "${DISTRIB_ID}" = "Ubuntu" ] && echo "LSB" && return
     [ ! -e /bin/rpm ] && echo "$dist" && return
     if rpm -q --whatprovides redhat-release >/dev/null 2>&1; then
         DISTRO_REL_RPM=$(rpm -q --whatprovides redhat-release 2>/dev/null | tail -n1)
@@ -100,7 +105,6 @@ function distro_version()
         DISTRO_REL_RPM=$(rpm -q --whatprovides suse-release 2>/dev/null | tail -n1)
 	    SUSE_RELEASE=1
     fi
-
     if [ -n "${FEDORA_RELEASE}" ]; then
 	    VER=$(rpm -q --qf "%{version}\n" ${DISTRO_REL_RPM})
 	    dist=fc${VER}
@@ -115,88 +119,133 @@ function distro_version()
     elif [ -n "${SUSE_RELEASE}" ]; then
 	    VER=$(rpm -q --qf "%{version}\n" ${DISTRO_REL_RPM})
 	    dist=suse${VER}
+    elif [ -n "${DISTRIB_ID}" ]; then
+	    dist=LSB
+    fi
+    echo "$dist"
+}
+
+function get_key()
+{
+    [ "$1" = "apt" ] && GPG_KEY[0]=${SERVER}/repo/GPG-KEY-libsmbios
+    TMPDIR=`mktemp -d /tmp/bootstrap.XXXXXX`
+    [ ! -d ${TMPDIR} ] && echo "Failed to make temporary directory." && exit 1
+    trap "rm -rf $TMPDIR" EXIT HUP QUIT
+
+    cd ${TMPDIR} 
+
+    i=0
+    while [ $i -lt ${#GPG_KEY[*]} ]; do
+	echo "Downloading GPG key: ${GPG_KEY[$i]}"
+	rm GPG-KEY > /dev/null 2>&1 || true
+	wget -q -O GPG-KEY ${GPG_KEY[$i]}
+	echo "    Importing key."
+	if [ "$1" = "rpm" ]; then
+	    rpm --import GPG-KEY
+	elif [ "$1" = "apt" ]; then
+	    apt-key add GPG-KEY
+	fi
+	if [ $? -ne 0 ]; then
+            echo "GPG-KEY import failed."
+            echo "   Either there was a problem downloading the key,"
+            echo "   or you do not have sufficient permissions to import the key."
+            exit 1
+	fi
+	i=$(( $i + 1 ))
+    done
+}
+
+function handle_rpms()
+{
+    get_key rpm
+    REPO_RPM="${REPO_NAME}-repository-${REPO_RPM_VER}.noarch.rpm"
+# download repo rpm
+    basearch=$(uname -i)
+    ACTUAL_REPO_URL=$(wget -q -O- ${SERVER}/${REPO_URL}/mirrors.pl?osname=${dist}\&basearch=$basearch | head -n1)
+    RPM_URL=${ACTUAL_REPO_URL}/${REPO_NAME}-repository/${REPO_RPM_VER}/$REPO_RPM
+    wget -q -N ${RPM_URL}
+    if [ ! -e ${REPO_RPM} ]; then
+	echo "Failed to download RPM: ${RPM_URL}"
+	exit 1
     fi
 
-    echo "$dist"
+    if [ "$CHECK_REPO_SIGNATURE" = "1" ]; then
+	rpm -K ${REPO_RPM} > /dev/null 2>&1
+	[ $? -ne 0 ] && echo "Failed ${REPO_RPM} GPG check!" && exit 1 
+    fi
+
+# write mirror cfg before installing repo rpm
+    write_mirror_cfg
+
+    echo "Installing ${REPO_RPM}"
+    rpm -U ${REPO_RPM} > /dev/null 2>&1
+
+    case $dist in
+	sles10)
+        #hw indep setup
+            FULL_URL=$(grep ^mirrorlist= /etc//yum.repos.d/dell-firmware-repository.repo | cut -d= -f2- )
+        # no vars in SLES, need to replace $basearch
+            basearch=$(uname -i)
+            FULL_URL=$(echo $FULL_URL | perl -p -i -e "s|\\\$basearch|$basearch|;")
+
+        # also sles doesnt support CGI params, so fake it with PATH_INFO 
+        # (supported by server-side cgi)
+            FULL_URL=$(echo $FULL_URL | perl -p -i -e "s|\?|/|;")
+
+        # SLES 10 doesnt support mirrorlist, so turn into redirect (support is in 
+        # server-side cgi for this)
+            FULL_URL=${FULL_URL}\&redirect=1\&redir_path=
+
+            yes | rug service-add -t ZYPP ${FULL_URL} dell-firmware-repository
+            rug subscribe dell-firmware-repository
+            ;;
+
+	*)
+            :
+            ;;
+    esac
+}
+
+function unknown_os()
+{
+    echo "Unable to determine that you are running an OS I know about."
+    echo "Handled OSs include Red Hat Enterprise Linux and CentOS,"
+    echo "Fedora Core and Novell SuSE Linux Enterprise Server and OpenSUSE,"
+    echo "and Ubuntu."
+}
+
+function handle_ubuntu()
+{
+    local list=/etc/apt/sources.list.d/dell-software-temp-bootstrap.list
+    rm -f ${list}
+    echo "deb http://linux.dell.com/repo ${DISTRIB_CODENAME} dell-software" > ${list}
+    get_key apt
+    aptitude update
+    aptitude install dell-repository-keys
+    aptitude install dell-firmware-repository dell-software-repository
+    rm -f ${list}
+    aptitude update
+}
+
+function handle_lsb()
+{
+    if [ "${DISTRIB_ID}" = "Ubuntu" ]; then
+	handle_ubuntu
+    else
+	unknown_os
+	exit 1
+    fi
 }
 
 dist=$(distro_version)
 if [ "${dist}" = "unknown" ]; then
-    echo "Unable to determine that you are running an OS I know about."
-    echo "Handled OSs include Red Hat Enterprise Linux and CentOS,"
-    echo "Fedora Core and Novell SuSE Linux Enterprise Server and OpenSUSE"
+    unknown_os
     exit 1
+elif [ "${dist}" != "LSB" ]; then
+    handle_rpms
+else
+    handle_lsb
 fi
-
-REPO_RPM="${REPO_NAME}-repository-${REPO_RPM_VER}.noarch.rpm"
-
-TMPDIR=`mktemp -d /tmp/bootstrap.XXXXXX`
-[ ! -d ${TMPDIR} ] && echo "Failed to make temporary directory." && exit 1
-trap "rm -rf $TMPDIR" EXIT HUP QUIT
-
-cd ${TMPDIR} 
-
-i=0
-while [ $i -lt ${#GPG_KEY[*]} ]; do
-    echo "Downloading GPG key: ${GPG_KEY[$i]}"
-    rm GPG-KEY > /dev/null 2>&1 || true
-    wget -q -O GPG-KEY ${GPG_KEY[$i]}
-    echo "    Importing key into RPM."
-    rpm --import GPG-KEY
-    if [ $? -ne 0 ]; then
-        echo "GPG-KEY import failed."
-        echo "   Either there was a problem downloading the key,"
-        echo "   or you do not have sufficient permissions to import the key."
-        exit 1
-    fi
-    i=$(( $i + 1 ))
-done
-
-# download repo rpm
-basearch=$(uname -i)
-ACTUAL_REPO_URL=$(wget -q -O- ${SERVER}/${REPO_URL}/mirrors.pl?osname=${dist}\&basearch=$basearch | head -n1)
-RPM_URL=${ACTUAL_REPO_URL}/${REPO_NAME}-repository/${REPO_RPM_VER}/$REPO_RPM
-wget -q -N ${RPM_URL}
-if [ ! -e ${REPO_RPM} ]; then
-    echo "Failed to download RPM: ${RPM_URL}"
-    exit 1
-fi
-
-if [ "$CHECK_REPO_SIGNATURE" = "1" ]; then
-    rpm -K ${REPO_RPM} > /dev/null 2>&1
-    [ $? -ne 0 ] && echo "Failed ${REPO_RPM} GPG check!" && exit 1 
-fi
-
-# write mirror cfg before installing repo rpm
-write_mirror_cfg
-
-echo "Installing ${REPO_RPM}"
-rpm -U ${REPO_RPM} > /dev/null 2>&1
-
-case $dist in
-    sles10)
-        #hw indep setup
-        FULL_URL=$(grep ^mirrorlist= /etc//yum.repos.d/dell-firmware-repository.repo | cut -d= -f2- )
-        # no vars in SLES, need to replace $basearch
-        basearch=$(uname -i)
-        FULL_URL=$(echo $FULL_URL | perl -p -i -e "s|\\\$basearch|$basearch|;")
-
-        # also sles doesnt support CGI params, so fake it with PATH_INFO 
-        # (supported by server-side cgi)
-        FULL_URL=$(echo $FULL_URL | perl -p -i -e "s|\?|/|;")
-
-        # SLES 10 doesnt support mirrorlist, so turn into redirect (support is in 
-        # server-side cgi for this)
-        FULL_URL=${FULL_URL}\&redirect=1\&redir_path=
-
-        yes | rug service-add -t ZYPP ${FULL_URL} dell-firmware-repository
-        rug subscribe dell-firmware-repository
-        ;;
-
-    *)
-        :
-        ;;
-esac
 
 echo "Done!"
 exit 0
